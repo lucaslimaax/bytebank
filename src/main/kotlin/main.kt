357223dbc32761f647f3688dae3ffa2b1dce3248
Lucas Lima <lucashendall@gmail.com>
2022-07-11 22:27:05 -0300

    modulo 2 - condicoes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,9 +4,24 @@ fun main(){
     val numeroConta: Int = 2000
     var saldo = 0.0
     saldo = 100 + 2.5
-    saldo += 200
+    saldo -= 200
 
     println("titular $titular")
     println("número da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    println("\n IF:")
+    if(saldo > 0){
+        println("Conta eh Positiva")
+    } else if(saldo == 0.0){
+        println("Conta eh Neutra")
+    } else{
+        println("Conta eh Negativa")
+    }
+    println("\n When:")
+    when {
+        saldo > 0.0 -> println("conta eh positiva")
+        saldo == 0.0 -> println("conta eh neutra")
+        else -> println("conta eh negativa")
+    }
 }
