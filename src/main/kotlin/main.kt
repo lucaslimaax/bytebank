370e5c6cdd20a6e43181b2838c477c036a986ae0
Lucas Lima <lucashendall@gmail.com>
2022-07-11 22:20:40 -0300

    modulo 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    println("Hello world")
+    println("Bem vindo ao Bytebank!!")
+    val titular: String = "Lucas"
+    val numeroConta: Int = 2000
+    var saldo = 0.0
+    saldo = 100 + 2.5
+    saldo += 200
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
 }
