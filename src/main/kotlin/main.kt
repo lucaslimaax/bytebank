d1f74de10ea88f8220592dd39a4c6b3a8362de2c
Lucas Lima <lucashendall@gmail.com>
2022-07-13 23:03:08 -0300

    modulo 3 - POO

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,33 +1,29 @@
 fun main() {
     println("Bem vindo ao Bytebank!!")
-    var i = 0
-    while (i < 5) {
-        val titular: String = "Lucas $i"
-        val numeroConta: Int = 2000 + i
-        var saldo = i + 10.0
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 100.0
 
-        println("titular $titular")
-        println("numero da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
-        i++
-    }
 
-//    for (i in 5 downTo 1 step 5) {
-//        if(i == 4){
-//            break
-//        }
-//        val titular: String = "Lucas $i"
-//        val numeroConta: Int = 2000 + i
-//        var saldo = i + 10.0
-//
-//        println("titular $titular")
-//        println("numero da conta $numeroConta")
-//        println("saldo da conta $saldo")
-//        println()
-//    }
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 2000
+    contaFran.saldo = 500.0
 
-//    testaCondicoes(saldo)
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
 }
 
 
@@ -48,4 +44,54 @@ fun testaCondicoes(saldo: Double) {
         else -> println("conta eh negativa")
     }
 
+}
+
+fun testaLacos(){
+    //    var i = 0
+//    while (i < 5) {
+//        val titular: String = "Lucas $i"
+//        val numeroConta: Int = 2000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("numero da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//        i++
+//    }
+
+    for (i in 1..5) {
+        if (i == 4) {
+            break
+        }
+        val titular: String = "Lucas $i"
+        val numeroConta: Int = 2000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
+//
+//    for (i in 5 downTo 1 step 2){
+//        val titular: String = "Lucas $i"
+//        val numeroConta: Int = 2000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("numero da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//    }
+
+
+    //usando breack e continue - e marcação de loop
+    loop@ for (i in 1..100) {
+        println("i $i")
+        for (j in 1..100) {
+            println("j $j")
+            if (j == 5) break@loop
+        }
+    }
 }
